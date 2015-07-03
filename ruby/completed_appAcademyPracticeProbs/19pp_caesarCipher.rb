3cf6861d42d3bf41cedc574effe47f09e68c0953
katrusso <russok2@rpi.edu>
2015-07-02 23:56:00 -0400

    completed #19, but not using suggested methods -- should reattempt when I have more time; QUESTION re: pass-by-ref in-line; added App Academy soln

--- ruby/completed_appAcademyPracticeProbs/19pp_caesarCipher.rb
+++ ruby/completed_appAcademyPracticeProbs/19pp_caesarCipher.rb
@@ -64,5 +64,26 @@ puts(
 =begin
 #APP ACADEMY SOLN
 
+def caesar_cipher(offset, string)
+  words = string.split(" ")
 
+  word_idx = 0
+  while word_idx < words.length
+    word = words[word_idx]
+
+    letter_idx = 0
+    while letter_idx < word.length
+      char_i = word[letter_idx].ord - "a".ord
+
+      new_char_i = (char_i + offset) % 26
+      word[letter_idx] = ("a".ord + new_char_i).chr
+
+      letter_idx += 1
+    end
+
+    word_idx += 1
+  end
+
+  return words.join(" ")
+end
 =end
