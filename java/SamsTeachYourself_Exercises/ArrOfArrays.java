759da648576c097567a83e87899f4e00eb9e348f
Kat Russo <acrimonster@gmail.com>
2015-10-25 10:20:28 -0400

    reminder, adjust indexes as the initial element in an array is 0, not 1.

--- java/SamsTeachYourself_Exercises/ArrOfArrays.java
+++ java/SamsTeachYourself_Exercises/ArrOfArrays.java
@@ -4,6 +4,11 @@ class ArrOfArrays {
 		System.out.println("# of elements in the first dimension: " + century.length); //100
 		System.out.println("# of elements in the second dimension: " + century[0].length); //52
 		System.out.println("# of elements in the third dimension: " + century[0][0].length); //7
+
+		String[][][] invisalign = new String[10][44][7]; //I have to use invisalign for 10 mos, which is 44 weeks at 7 days a week. Each day in that 7 day week has a value (a date).
+		invisalign[9][43][6] = "19-Oct-15"; //remember to adjust the indexes as they range from 0 to n-1 in arrays
+		String firstDay= invisalign[9][43][6];
+		System.out.println("My first day in the countdown of having invisalign was: " + firstDay);
 	}
 }
 /*
