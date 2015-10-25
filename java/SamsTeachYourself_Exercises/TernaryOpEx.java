f8c64993d4aa65fcd6f7cb8851384e9de66f2c58
Kat Russo <acrimonster@gmail.com>
2015-10-25 10:52:49 -0400

    Conditional Operator (aka ternary operator) example and notes.

---
+++ java/SamsTeachYourself_Exercises/TernaryOpEx.java
@@ -0,0 +1,49 @@
+//Source: http://alvinalexander.com/java/edu/pj/pj010018
+
+public class TernaryOpEx
+{
+  /**
+   * Examples using the Java ternary operator
+   * @author alvin alexander, devdaily.com
+   */
+  public static void main(String[] args)
+  {
+    // min value example
+    int minVal, a=3, b=2;
+    minVal = a < b ? a : b;
+    System.out.println("min = " + minVal);
+     
+    // absolute value example
+    a = -10;
+    int absValue = (a < 0) ? -a : a;
+    System.out.println("abs = " + absValue);
+ 
+    // result is assigned the value 1.0
+    float result = true ? 1.0f : 2.0f;
+    System.out.println("float = " + result);
+  
+    // result is assigned the value "Sorry Dude, it's false"
+    String s = false ? "Dude, that was true" : "Sorry Dude, it's false";
+    System.out.println(s);
+     
+    // example using the ternary operator on the rhs, in a string
+    int x = 5;
+    String out = "There " + (x > 1 ? " are " + x + " cookies" : "is one cookie") + " in the jar.";
+    System.out.println(out);
+
+
+  }
+}
+    //returnString = "Thank you " + (person.isMale() ? "Mr. " : "Ms. ") + person.getLastName() + ".";
+    //myFunc( (COND ? defaultValue : getMyFuncParameter()) );
+ 
+
+ /*
+    The "IF (COND) THEN Statement(s) ELSE Statement(s)" construct is, itself, a statement.
+
+    The "COND ? Statement : Statement" construct, however, is an expression, and therefore 
+    it can sit on the right-hand side (rhs) of an assignment.
+
+    e.g. 
+        myFunc( (COND ? defaultValue : getMyFuncParameter()) );
+ */
