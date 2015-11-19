0ea83e9e9c117e00b00786096948b58d98da9782
katrusso <hq@Mains-MacBook-Pro.local>
2015-11-19 12:27:14 -0500

    practice with arrays and streams.

---
+++ java/SamsTeachYourselfJava6/Streams/Input/ByteReader.java
@@ -0,0 +1,23 @@
+import java.io.*; //contains IOExceptions (Stream exceptions)
+
+public class ByteReader {
+	public static void main(String[] args) {
+		try {
+			FileInputStream file = new FileInputStream("class.dat");
+			boolean eof = false;
+			int count = 0;
+			while (!eof) {
+				int input = file.read();
+				System.out.print(input + " ");
+				if (input == -1)
+					eof = true;
+					else
+						count++;
+			}
+			file.close();
+			System.out.prinln("\n Bytes read: " + count);
+		} catch (IOException e) {
+			System.out.println(("Error -- " + e.toString()));
+		}
+	}
+}
